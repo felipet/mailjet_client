1ad093b177f0e761719dbf722df3e2d80f794e47
Felipe Torres González <torresfelipex1@gmail.com>
2024-10-02 11:47:50 +0200

    Refactor response objects

--- src/data_objects/request.rs
+++ src/data_objects/request.rs
@@ -3,6 +3,8 @@
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
+//! Translations of data objects use as params for the endpoints of Mailjet's API.
+
 use crate::data_objects::{Message, MessageProperty};
 use serde::Serialize;
 
@@ -13,4 +15,21 @@ pub struct SendEmailParams<'a> {
     pub advance_error_handling: Option<bool>,
     pub globals: Option<MessageProperty<'a>>,
     pub messages: Vec<Message>,
+}
+
+#[derive(Serialize, Debug, Clone, Default)]
+#[serde(rename_all = "PascalCase")]
+pub struct SenderQuery {
+    #[serde(rename = "DnsID")]
+    pub dns_id: Option<i64>,
+    pub domain: Option<String>,
+    pub email: Option<String>,
+    pub is_domain_sender: Option<bool>,
+    pub local_part: Option<String>,
+    pub show_deleted: Option<bool>,
+    pub status: Option<String>,
+    pub limit: Option<u16>,
+    pub offset: Option<u16>,
+    pub count_only: Option<bool>,
+    pub sort: Option<String>,
 }
--- src/data_objects/responses.rs
+++ src/data_objects/responses.rs
@@ -3,10 +3,50 @@
 // This work is licensed under the terms of the MIT license.
 // For a copy, see <https://opensource.org/licenses/MIT>.
 
-//! Data objects related to the endpoint `/sender` of Mailjet's REST API.
+//! Translations of data objects returned by the endpoints of Mailjet's API.
+
 use crate::data_objects::ResponseObject;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
+
+/// Data object returned by `/send` (v3.1) as `Messages`. See [`/send`][send].
+/// [send]: https://dev.mailjet.com/email/reference/send-emails#v3_1_post_send
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct SendResponseObject {
+    pub status: ResponseStatus,
+    pub errors: Option<Vec<ResponseError>>,
+    pub to: Option<Vec<MessageError>>,
+    pub cc: Option<Vec<MessageError>>,
+    pub bcc: Option<Vec<MessageError>>,
+}
+
+impl ResponseObject for SendResponseObject {}
+
+/// Data object for the field `Errors` in the response of `/send`. See [`/send`][send].
+/// [send]: https://dev.mailjet.com/email/reference/send-emails#v3_1_post_send
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct ResponseError {
+    pub error_identifier: Uuid,
+    pub error_code: String,
+    pub status_code: i64,
+    pub error_message: String,
+    pub error_related_to: Vec<String>,
+}
+
+/// Data object for the field `To`, `Bcc`, `Cc` in the response of `/send`. See [`/send`][send].
+/// [send]: https://dev.mailjet.com/email/reference/send-emails#v3_1_post_send
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct MessageError {
+    pub email: String,
+    #[serde(rename = "MessageUUID")]
+    pub message_uuid: Uuid,
+    #[serde(rename = "MessageID")]
+    pub message_id: i64,
+    pub message_href: String,
+}
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
 #[serde(rename_all = "lowercase")]
@@ -25,6 +65,13 @@ pub enum Status {
     Deleted,
 }
 
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "lowercase")]
+pub enum ResponseStatus {
+    Success,
+    Error,
+}
+
 #[derive(Deserialize, Default, Debug, Clone)]
 #[serde(rename_all = "PascalCase")]
 pub struct Sender {
@@ -39,68 +86,4 @@ pub struct Sender {
     #[serde(rename = "ID")]
     pub id: i64,
     pub status: Status,
-}
-
-impl ResponseObject for Sender {}
-
-#[derive(Serialize, Debug, Clone, Default)]
-#[serde(rename_all = "PascalCase")]
-pub struct SenderQuery {
-    #[serde(rename = "DnsID")]
-    pub dns_id: Option<i64>,
-    pub domain: Option<String>,
-    pub email: Option<String>,
-    pub is_domain_sender: Option<bool>,
-    pub local_part: Option<String>,
-    pub show_deleted: Option<bool>,
-    pub status: Option<String>,
-    pub limit: Option<u16>,
-    pub offset: Option<u16>,
-    pub count_only: Option<bool>,
-    pub sort: Option<String>,
-}
-
-// #[derive(Debug)]
-// pub struct SendResponse {
-//     pub messages: SendResponseObject,
-// }
-
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "PascalCase")]
-pub struct SendResponseObject {
-    pub status: ResponseStatus,
-    pub errors: Option<Vec<ResponseError>>,
-    pub to: Option<Vec<MessageError>>,
-    pub cc: Option<Vec<MessageError>>,
-    pub bcc: Option<Vec<MessageError>>,
-}
-
-impl ResponseObject for SendResponseObject {}
-
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "lowercase")]
-pub enum ResponseStatus {
-    Success,
-    Error,
-}
-
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "PascalCase")]
-pub struct ResponseError {
-    pub error_identifier: Uuid,
-    pub error_code: String,
-    pub status_code: i64,
-    pub error_message: String,
-    pub error_related_to: Vec<String>,
-}
-
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "PascalCase")]
-pub struct MessageError {
-    pub email: String,
-    #[serde(rename = "MessageUUID")]
-    pub message_uuid: Uuid,
-    #[serde(rename = "MessageID")]
-    pub message_id: i64,
-    pub message_href: String,
 }
