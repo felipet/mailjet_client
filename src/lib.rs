eaa9e2eec94a9a4f81153cdc64d54d73c6fe7707
Felipe Torres González <torresfelipex1@gmail.com>
2024-10-12 11:25:53 +0200

    Improved lib docs

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@
 //! - **Tracing support via [Tracing](https://crates.io/crates/tracing)**: the library code includes tracing calls
 //!   using `Tracing`'s API.
 //! - **Usage of [Reqwest](https://crates.io/crates/reqwest) as internal HTTP client**: `Reqwest` is my crate of
-//!   choice for this use-case scenarios. The crate `Reqwest-tracing` is also addedd to enable tracing support for
+//!   choice for this use-case scenarios. The crate `Reqwest-tracing` is also added to enable tracing support for
 //!   the internal HTTP client.
 //! - **High level of test coverage** and support for CI. Given that I aim to include this crate into another service
 //!   that needs a high level of reliavility, not including a proper set of tests was a non-go.
@@ -29,19 +29,85 @@
 //! develop it by yourself, or you need somebody else to do it, it will be good to know that there's interest on
 //! adding such missing feature to the client.
 //!
+//! # Library Structure
+//!
+//! The library includes a module that implements the logic to send and receive HTTP requests to the external REST API.
+//! Depending on the target endpoint of that API, the request body parameters and the response object will vary.
+//! The library's code attempts to abstract such thing as much as possible using traits. Visit the documentation page
+//! for the module [crate::data_objects] to read a full explanation. In brief, two main objects are distinguish:
+//!
+//! ## Response Objects
+//!
+//! Responses object vary depending on the used endpoint. Whilst endpoints that implement the API v3.1 include
+//! a lot of information about errors that might ocurred; endpoints that implement the API v3.0 only include a brief
+//! error code when errors ocurred. Aside from that, the returned objects as the payload of the response vary quite
+//! a lot.
+//!
+//! To offer a client as homogeneous as possible, all the client's member functions return a trait object:
+//! [crate::data_objects::ResponseObject]. Then, `struct`s implement a particular response object, and you'll need to
+//! cast from the trait object to a particular object based on the endpoint that you are using.
+//!
+//! ## Request Objects
+//!
+//! The same applies to request objects: client's member function expect a trait object that implements the trait
+//! [crate::data_objects::RequestObject]. Then, is the client's implementation who should cast such thing to the
+//! particular object expected by the external endpoint.
+//!
+//! # Usage
+//!
+//! The current client only supports sending emails, either using Mailjet's API v3 or v3.1.
+//!
+//! To start sending emails, instantiate the client [crate::MailjetClient] either directly using
+//! [crate::MailjetClient::new] or (best choice) using the object [crate::MailjetClientBuilder], which eases the
+//! construction of a new client.
+//!
+//! At a bare minimum, you have to provide your API credentials to the client. Check [crate::MailjetClient::new]'s doc
+//! to get more details about those credentials. The rest of parameters are optional.
+//!
+//! ```rust,ignore
+//! let client = MailjetClientBuilder::new(api_user, api_key)
+//!     .build()
+//!     .expect("Failed to build a new Mailjet client");
+//! ```
+//!
+//! The builder object implements a fluent interface so add as many attributes as you wish to the builder object, and
+//! once your done with it, call `.build()` to complete the process.
+//!
+//! Then, composed a new email using either [crate::data_objects::SimpleMessage] (for API v3.0) or
+//! [crate::data_objects::SendEmailParams] (for API v3.1) and call the send method:
+//!
+//! ```rust, ignore
+//! let message = //...;
+//!
+//! let response = client.send_email(&message).await?;
+//! ```
+//!
+//! Do you need more examples? Visit the *examples* folder to get a detailed view of the usage of this crate.
+//!
+//!
 //! [mapi]: https://dev.mailjet.com/email/reference/overview/
 //! [reqwest]: https://crates.io/crates/reqwest
 //! [tracing]: https://crates.io/crates/tracing
 
-/// This module includes all the object definitions need to interact with Mailjet's API.
+/// This module includes all the object definitions need to interact with the external API data objects.
 ///
 /// # Description
 ///
 /// The objects defined in the REST [API][mapi] are directly translated into Rust's types using naming conventions of
-/// Rust (snake case for `struct`'s members). Be aware of that, because some names in the objects defined in the REST
+/// Rust (snake case for `struct`'s members). Be aware of that, because some names of the objects defined in the REST
 /// API don't follow any known convention. _PascalCase_ seems to be the usual choice for them, but some object's
-/// atributes get akward names such as ***DnsID***. The objects defined here in strictly follow the snake case naming
+/// attributes get awkward names such as ***DnsID***. The objects defined here in strictly follow the snake case naming
 /// rules, and `serde`'s macros are used to rename the fields (see [this](https://serde.rs/field-attrs.html#rename)).
+///
+/// # Simple Translation Table
+///
+/// This is a simple table that links what objects you should use from this crate when you target a particular
+/// endpoint of the external API:
+///
+/// |    Endpoint   | Request object | Response object |
+/// |:-------------:|:---------------|:----------------|
+/// | `/send` v3    | [crate::data_objects::SimpleMessage] | [crate::data_objects::MessageObject] |
+/// | `/send` v3.1  | [crate::data_objects::SendEmailParams] | [crate::data_objects::SendResponseObject] |
 ///
 /// # Mailjet REST API responses
 ///
@@ -72,6 +138,9 @@
 ///
 /// [mapi]: https://dev.mailjet.com/email/reference/overview/
 pub mod data_objects {
+
+    use std::any::Any;
+
     /// An alias to ease developers difficult lives: a [Vec] of objects that implement [ResponseObject].
     pub type ObjectType = Vec<Box<dyn ResponseObject>>;
 
@@ -97,11 +166,7 @@ pub mod data_objects {
     ///
     /// Every response from the API shall include a matching type in this crate that implements this trait.
     /// This is mandatory to return a generic [Response] from all the client calls provided by this client.
-    pub trait ResponseObject: std::fmt::Debug {
-        // fn as_any(&self) -> &dyn Any;
-    }
-
-    use std::any::Any;
+    pub trait ResponseObject: std::fmt::Debug {}
 
     /// Trait that identifies any object that is used as parameters for a request to the external API.
     pub trait RequestObject: std::fmt::Debug {
